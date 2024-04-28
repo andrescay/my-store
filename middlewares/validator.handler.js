fe0063dd72e245c9343e7dab9883b07c6542be2e
hsk <cristiandres4444@hotmail.com>
2024-04-28 13:27:07 -0500

    DEB:limpiar console.log

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -4,8 +4,6 @@ function validatorHandler (eschema, property){
   // Se crea una función que retorna middlewares "dinámicos"
   return (req, res, next) =>{
     const data = req[property]
-    console.log('***************')
-    console.log(data)
     const { error } = eschema.validate(data, {abortEarly: false})
     if(error){
       next(boom.badRequest(error))
